72deb16188656006f0a2121df3c3d8970ce5f9ed
Stepan <Az7081488228>
2020-02-01 11:59:41 +0300

    third

--- app/src/main/java/com/example/searchtwo/Adapter.java
+++ app/src/main/java/com/example/searchtwo/Adapter.java
@@ -1,4 +1,99 @@
 package com.example.searchtwo;
 
-public class Adapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    private int[] mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    Adapter(Context context, int[] data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the cell layout from xml when needed
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.frament_cell1, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each cell
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //holder.myTextView.setText(mData[position]);
+        if (mData[position] == 1){
+            holder.picture.setImageResource(R.drawable.bird);
+        }if (mData[position] == 2){
+            holder.picture.setImageResource(R.drawable.catthum);
+        }if (mData[position] == 3){
+            holder.picture.setImageResource(R.drawable.demon);
+        }if (mData[position] == 4){
+            holder.picture.setImageResource(R.drawable.knifeugroza);
+        }if (mData[position] == 5){
+            holder.picture.setImageResource(R.drawable.pinguin);
+        }if (mData[position] == 6){
+            holder.picture.setImageResource(R.drawable.poc);
+        }if (mData[position] == 7){
+            holder.picture.setImageResource(R.drawable.racoon);
+        }if (mData[position] == 8){
+            holder.picture.setImageResource(R.drawable.svidetel);
+        }
+        else {
+            holder.picture.setImageResource(R.drawable.ic_launcher_foreground);
+        }
+
+    }
+
+    // total number of cells
+    @Override
+    public int getItemCount() {
+        return mData.length;
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+        ImageView picture;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            picture = itemView.findViewById(R.id.picture);
+            myTextView = itemView.findViewById(R.id.info_text);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    int getItem(int id) {
+        return mData[id];
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
 }
--- app/src/main/java/com/example/searchtwo/MainActivity.java
+++ app/src/main/java/com/example/searchtwo/MainActivity.java
@@ -1,23 +1,41 @@
 package com.example.searchtwo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
-    int i[] = {1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8};
+public class MainActivity extends AppCompatActivity implements Adapter.ItemClickListener {
+    Adapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        shuffleArray(i);
         setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        int[] data = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8};
+        shuffleArray(data);
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvNumbers);
+        int numberOfColumns = 4;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        adapter = new Adapter(this, data);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+    @Override
+    public void onItemClick(View view, int position) {
+        Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
     }
     static void shuffleArray(int[] ar) {
         Random rnd = new Random();
-        for (int i = ar.length - 1; i > 0; i--) {
+       for (int i = ar.length - 1; i > 0; i--) {
             int index = rnd.nextInt(i + 1);
             // Simple swap
             int a = ar[index];
@@ -25,54 +43,72 @@ public class MainActivity extends AppCompatActivity {
             ar[i] = a;
         }
     }
-    public Integer Cell_1(){
-        return Integer.valueOf(i[0]);
-    }
-    public Integer Cell_2(){
-        return Integer.valueOf(i[1]);
-    }
-    public Integer Cell_3(){
-        return Integer.valueOf(i[2]);
-    }
-    public Integer Cell_4(){
-        return Integer.valueOf(i[3]);
-    }
-    public Integer Cell_5(){
-        return Integer.valueOf(i[4]);
-    }
-    public Integer Cell_6(){
-        return Integer.valueOf(i[5]);
-    }
-    public Integer Cell_7(){
-        return Integer.valueOf(i[6]);
-    }
-    public Integer Cell_8(){
-        return Integer.valueOf(i[7]);
-    }
-    public Integer Cell_9(){
-        return Integer.valueOf(i[8]);
-    }
-    public Integer Cell_10(){
-        return Integer.valueOf(i[9]);
-    }
-    public Integer Cell_11(){
-        return Integer.valueOf(i[10]);
-    }
-    public Integer Cell_12(){
-        return Integer.valueOf(i[11]);
-    }
-    public Integer Cell_13(){
-        return Integer.valueOf(i[12]);
-    }
-    public Integer Cell_14(){
-        return Integer.valueOf(i[13]);
-    }
-    public Integer Cell_15(){
-        return Integer.valueOf(i[14]);
-    }
-    public Integer Cell_16(){
-        return Integer.valueOf(i[15]);
-    }
+//    int i[] = {1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8};
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        shuffleArray(i);
+//        setContentView(R.layout.activity_main);
+//    }
+//    static void shuffleArray(int[] ar) {
+//        Random rnd = new Random();
+//        for (int i = ar.length - 1; i > 0; i--) {
+//            int index = rnd.nextInt(i + 1);
+//            // Simple swap
+//            int a = ar[index];
+//            ar[index] = ar[i];
+//            ar[i] = a;
+//        }
+//    }
+//    public Integer Cell_1(){
+//        return Integer.valueOf(i[0]);
+//    }
+//    public Integer Cell_2(){
+//        return Integer.valueOf(i[1]);
+//    }
+//    public Integer Cell_3(){
+//        return Integer.valueOf(i[2]);
+//    }
+//    public Integer Cell_4(){
+//        return Integer.valueOf(i[3]);
+//    }
+//    public Integer Cell_5(){
+//        return Integer.valueOf(i[4]);
+//    }
+//    public Integer Cell_6(){
+//        return Integer.valueOf(i[5]);
+//    }
+//    public Integer Cell_7(){
+//        return Integer.valueOf(i[6]);
+//    }
+//    public Integer Cell_8(){
+//        return Integer.valueOf(i[7]);
+//    }
+//    public Integer Cell_9(){
+//        return Integer.valueOf(i[8]);
+//    }
+//    public Integer Cell_10(){
+//        return Integer.valueOf(i[9]);
+//    }
+//    public Integer Cell_11(){
+//        return Integer.valueOf(i[10]);
+//    }
+//    public Integer Cell_12(){
+//        return Integer.valueOf(i[11]);
+//    }
+//    public Integer Cell_13(){
+//        return Integer.valueOf(i[12]);
+//    }
+//    public Integer Cell_14(){
+//        return Integer.valueOf(i[13]);
+//    }
+//    public Integer Cell_15(){
+//        return Integer.valueOf(i[14]);
+//    }
+//    public Integer Cell_16(){
+//        return Integer.valueOf(i[15]);
+//    }
 }
 
 //Handler handler = new Handler();
